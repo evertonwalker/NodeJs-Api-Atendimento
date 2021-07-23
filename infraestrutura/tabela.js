88180641e1279a5502ddc692eec406661e6b79e3
Everton Walker <everton.walker@hotmail.com>
2021-07-23 16:37:44 -0300

    moment, retorno dos erros

--- infraestrutura/tabela.js
+++ infraestrutura/tabela.js
@@ -9,7 +9,11 @@ class Tabelas {
         const sql = `CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL
         AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20),
         servico varchar(20) NOT NULL, status varchar(20) NOT NULL,
+        dataCriacao datetime NOT NULL,
+        data varchar(30) NOT NULL,
         observacoes text, PRIMARY KEY(id))`
+
+        // const sql = 'drop table Atendimentos';
 
         this.conexao.query(sql, err => {
             if (err) {
--- models/atendimento.js
+++ models/atendimento.js
@@ -1,14 +1,19 @@
 const conexao = require('../infraestrutura/conexao')
-
+const moment = require('moment');
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        
         const sql = 'INSERT INTO Atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        const atendimentoDatado = { ...atendimento, dataCriacao, data }
+
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if (erro) {
-                console.log(erro)
+                res.status(400).json(erro)
             } else {
-                console.log(resultados)
+                res.status(200).json(resultados)
             }
         })
     }
